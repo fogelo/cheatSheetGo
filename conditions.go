da756faaf9d6827e8f6afea254669b2ddd33708a
antor <OantonS@yandex.ru>
2023-05-26 10:58:40 +0300

    conditions

---
+++ conditions.go
@@ -0,0 +1,19 @@
+// § Массивы
+
+package main
+
+import "fmt"
+
+func main() {
+	a := 5
+	b := 6
+	if a < b {
+		fmt.Println("a is less than b")
+	} else if a > b {
+		fmt.Println("a is more than b")
+	} else {
+		fmt.Println("a равно b")
+	}
+}
+
+// есть еще конструкция switch case
